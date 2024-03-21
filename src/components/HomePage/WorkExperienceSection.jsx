45b12c79987ea63f9112ceb37339bc2e64b4d0fe
codebymojnu <thisismojnu@gmail.com>
2024-03-21 20:36:04 +0600

    responsiblities add

--- src/components/HomePage/WorkExperienceSection.jsx
+++ src/components/HomePage/WorkExperienceSection.jsx
@@ -5,34 +5,53 @@ export default function WorkExperienceSection() {
       position: "Office Assistant",
       institution: "PUST EDGE Project, Department of CSE",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
+        "As a participant in the project-based course organized by the Department of Computer Science and Engineering, I am actively involved in various aspects of student data management and course administration. My primary responsibilities include overseeing student data, managing attendance records, and providing technical support to course participants.",
+      responsibilities: [
+        "Student Data Management",
+        "Attendance Management",
+        "Student Interaction and Support",
+        "Teacher Interaction and Support",
+      ],
     },
     {
-      year: "2020-2022",
+      year: "2017 - 2023",
       position: "Chemistry Tutor",
       institution: "XYZ Academy",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
+        "As a participant in the project-based course organized by the Department of Computer Science and Engineering, I am actively involved in various aspects of student data management and course administration. My primary responsibilities include overseeing student data, managing attendance records, and providing technical support to course participants.",
+      responsibilities: [
+        "Conducting chemistry tutoring sessions",
+        "Preparing study materials and lesson plans",
+        "Assessing student progress and providing feedback",
+      ],
     },
     // Add more work experience entries as needed
   ];
 
   return (
-    <section className="">
+    <section className="py-4">
       <div className="container mx-auto px-4">
         <div className="mb-8">
           <h2 className="text-xl font-bold mb-4 pb-2 border-b border-gray-300">
             Work Experience
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-y-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {workExperience.map((item, index) => (
-              <div key={index} className="flex">
-                <p className="mr-4">{item.year}</p>
-                <div>
-                  <p className="font-bold">{item.position}</p>
-                  <p>{item.institution}</p>
-                  <p className="text-gray-700">{item.description}</p>
-                </div>
+              <div key={index} className="bg-white rounded-lg shadow-md p-6">
+                <p className="text-gray-700 font-bold">{item.year}</p>
+                <p className="text-lg font-semibold mt-2">{item.position}</p>
+                <p className="text-gray-600">{item.institution}</p>
+                <p className="text-gray-700 mt-2">{item.description}</p>
+                <h4 className="text-md font-bold mt-2">
+                  Key Responsibilities:
+                </h4>
+                <ul className="mt-1">
+                  {item.responsibilities.map((responsibility, idx) => (
+                    <li key={idx} className="text-gray-700">
+                      - {responsibility}
+                    </li>
+                  ))}
+                </ul>
               </div>
             ))}
           </div>
