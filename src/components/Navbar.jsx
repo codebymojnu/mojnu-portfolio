558bee0e0473ab77074408262c934b5fd39bf4de
codebymojnu <thisismojnu@gmail.com>
2024-03-30 23:51:25 +0600

    navbar edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ function Navbar() {
 
   return (
     <nav className="">
-      <div className="max-w-7xl mx-auto px-4 py-2">
+      <div className="container mx-auto p-4 w-full md:max-w-4xl  m-2 mt-0 md:p-8">
         <div className="flex justify-between items-center">
           <NavLink to="/">
             <div className="flex-shrink-0">Home</div>
