ba064c5e9a71c9fca758b5d1a28235f5d311e6f3
codebymojnu <thisismojnu@gmail.com>
2024-11-21 11:31:07 +0600

    add some text

--- src/components/HomePage/AreaOfInterest.jsx
+++ src/components/HomePage/AreaOfInterest.jsx
@@ -1,8 +1,7 @@
 export default function AreaOfInterestSection() {
   const interests = [
     "Web and Mobile Application Development",
-    "Content Creation",
-    "Artificial Intelligence",
+    "Content Creation, Artificial Intelligence",
     "Blogging",
   ];
 
@@ -13,10 +12,10 @@ export default function AreaOfInterestSection() {
           <h2 className="text-xl font-bold mb-4 pb-2 border-b border-gray-300">
             Area of Interest
           </h2>
-          <div className="grid grid-cols-2 md:grid-cols-4 lg:flex lg:flex-wrap">
+          <div className="lg:flex lg:flex-wrap">
             {interests.map((interest, index) => (
               <div key={index} className="rounded-md">
-                <p className="text-gray-800 mr-2">{interest + ","}</p>
+                <sapn className="text-gray-800 mr-2">{interest + ","}</sapn>
               </div>
             ))}
           </div>
